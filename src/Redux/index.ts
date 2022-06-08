f333757b4eb56cba81f95a78c2bb73ca6ec34ecf
Neider Pandales <napandales@indracompany.com>
2022-06-08 06:00:37 -0500

    chore: instalando redux

---
+++ src/Redux/index.ts
@@ -0,0 +1,7 @@
+const types = {
+
+}
+
+export {
+  types
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import Home from './Pages/Home';
+import { store } from './Redux/Store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Home />
+    <Provider store={store}>
+      <Home />
+    </Provider>
   </React.StrictMode>
 );
